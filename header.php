2c79f91fc5e3ad045ba0a607dc96888ce696eed0
Nickkos <Nickkoskowski@gmail.com>
2014-03-03 21:37:58 -0800

    Added Header and some class and font definitions.

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <html>
 <head>
 	<title></title>
@@ -7,3 +8,27 @@
 	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<div class="container">
+		<div class="header">
+			<div class="inner-wrapper">
+				<p class="logo">
+					<img src="images/logo.png">
+				</p>
+				<p class="left-align">78 2ND HOUSE RD MONTAUK, NY, 11954 <br/>CONTACT@BARBERRY.COM</p>
+				<p class="right-align">
+					<button>My Account</button>
+					<button>English</button>
+				</p>
+			</div>
+			<div class="navigation">
+					<ul>
+						<li>Home</li>
+						<li>Features</li>
+						<li>About Us</li>
+						<li>Portfolio</li>
+						<li>Blog</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+		</div>
+	</div>
