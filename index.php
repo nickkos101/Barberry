2ee72556a05eeeab1c954820c78cb58483ef9830
Nickkos <Nickkoskowski@gmail.com>
2014-03-03 22:42:39 -0800

    Updated index, slider and widget models.

--- index.php
+++ index.php
@@ -1,3 +1,79 @@
 <?php include 'header.php'; ?>
+<div class="slider">
+	<div class="container">
+		<div class="slide">
+			<img src="images/slide.png">
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="hp-content">
+		<div class="half-column">
+			<div class="box">
+				<div class="inner-wrapper">
+					<h2>GET 25% OFF WHEN YOU SPEND $100</h2>
+				</div>
+			</div>
+		</div><div class="half-column">
+		<div class="box">
+			<div class="inner-wrapper">
+				<h2>FREE SHIPPING ON ALL ORDERS</h2>
+			</div>
+		</div>
+	</div>
+	<div class="widget">
+		<h3>Products</h3>
+		<div class="product">
+			<img src="images/product.jpg">
+			<h4>Product Name</h4>
+			<p>$1450.00</p>
+		</div>
+		<div class="product">
+			<img src="images/product.jpg">
+			<h4>Product Name</h4>
+			<p>$1450.00</p>
+		</div>
+		<div class="product">
+			<img src="images/product.jpg">
+			<h4>Product Name</h4>
+			<p>$1450.00</p>
+		</div>
+		<div class="product">
+			<img src="images/product.jpg">
+			<h4>Product Name</h4>
+			<p>$1450.00</p>
+		</div>
+	</div>
+	<div class="widget">
+		<h3>Brands</h3>
+		<div class="brands">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+			<img src="images/brand.jpg">
+		</div>
+	</div>
+	<div class="widget">
+		<h3>Latest News</h3>
+		<div class="third-column">
+			<h4>This is a headline</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut.</p>
+		</div>
+
+		<div class="third-column">
+			<h4>This is a headline</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut.</p>
+		</div>
+		<div class="third-column">
+			<h4>This is a headline</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+		</div>
+	</div>
+</div>
+</div>
 
 <?php include 'footer.php'; ?>
