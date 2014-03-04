7afb02637b1502786d1ea2de94b53458fa377192
Sara Herman <saraherman92@gmail.com>
2014-03-03 21:33:23 -0800

    Footer copyright area
    added footer copyright area

--- footer.php
+++ footer.php
@@ -1,2 +1,15 @@
+	<div class="footer">
+		<div class="container">
+			<div class="column">
+
+			</div>
+
+			<div class="copyright">
+				<p>&copy;2013 - Barberry woocommerce theme. created by temashdesign</p>
+				<img src="images/credit_cards.png" alt="credit cards" class="right">
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<title></title>
 	<link href='normalize.css' type='text/css'>
 	<link href='nick.css' rel='stylesheet' type='text/css'>
-	<link href='sara.css' type='text/css'>
+	<link href='sara.css' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
