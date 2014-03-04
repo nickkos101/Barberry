fcd320bcb77615f5e2546a560f1f2f1897e87bdd
Sara Herman <saraherman92@gmail.com>
2014-03-03 22:40:11 -0800

    added footer columns
    footer columns

--- footer.php
+++ footer.php
@@ -1,7 +1,45 @@
 	<div class="footer">
 		<div class="container">
 			<div class="column">
+				<h1>about us</h1>
+				<img src="images/bar_icon.jpg" alt="shopping bag">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis que porta sapien nec leo convallis.</p>
+				<p>t: 1-866-950-1638</p>
+				<p>f: 1-866-950-1639</p>
+				<p>e: <a href="mailto:info@yourcompany.com">contact@barberry.com</a></p>
+			</div>
+			<div class="column">
+				<h1>top rated products</h1>
+				<ul>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+				</ul>
+			</div>
+			<div class="column">
+				<h1>on sale</h1>
+				<ul>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+					<li>
+						<a href=""><img src="images/trench.jpg" alt="Brit 'Mottram' Poplin Trench">Brit 'Mottram' Poplin Trench</a>
+					</li>
+				</ul>
+			</div>
+			<div class="column">
+				<h1>product tags</h1>
 
+				<h1>barberry social</h1>
 			</div>
 
 			<div class="copyright">
